f7c46daf5aacb3bdb18db16849203a05a6aca9f1
TomislavP98 <tomislav.petrov98@gmail.com>
2020-12-09 19:00:42 +0100

    Reorganisation

--- Vjezba3/src/components/Combobox/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
- 
-import styles from './style.module.css' 
-
-const Combobox = () => <span className={styles.combobox}>English</span>
-
-export default Combobox
--- Vjezba3/src/constants/const.js
+++
@@ -1,1 +0,0 @@
-export const navs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
